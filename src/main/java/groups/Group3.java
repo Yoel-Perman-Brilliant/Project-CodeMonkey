f6d45623792c4074a71da64271f07564e3c4b565
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-06-12 16:53:19 +0300

    did conventions

--- src/main/java/groups/Group3.java
+++ src/main/java/groups/Group3.java
@@ -4,12 +4,10 @@ import frc.robot.utils.ProgramBase;
 
 /**
  * <H2>Here you should write your program</H2>you can use the methods: <br>
- * <p>
- * {@link  #moveForwardInSeconds(double)} <br>
+ * <p>{@link  #moveForwardInSeconds(double)} <br>
  * {@link  #moveBackwardInSeconds(double)} <br>
  * {@link  #turnLeftInDegrees(double)} <br>
- * {@link  #turnRightInDegrees(double)} <br>
- * </p>
+ * {@link  #turnRightInDegrees(double)} <br></p>
  * <p>
  * In the basic program below, the robot will <b>drive forward</b> for 2 seconds, and then <b>turn left</b> by 90 degrees<br>
  * <p>
@@ -17,6 +15,7 @@ import frc.robot.utils.ProgramBase;
  * Have fun!
  */
 public class Group3 extends ProgramBase {
+
     @Override
     public void writeProgram() {
         moveForwardInSeconds(2); //todo remove this line and start coding
