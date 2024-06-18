070c366fe3c8a2f1b3eac60750346dba6689885e
turtleminecraft <schnitzelgirl69@gmail.com>
2024-06-18 08:36:38 +0300

    group 3

--- src/main/java/groups/Group3.java
+++ src/main/java/groups/Group3.java
@@ -20,5 +20,7 @@ public class Group3 extends ProgramBase {
     public void writeProgram() {
         moveForwardInSeconds(2); //todo remove this line and start coding
         turnLeftInDegrees(90); //todo remove this line and start coding
+        turnLeftInDegrees(90); //todo remove this line and start coding
+        turnLeftInDegrees(90); //todo remove this line and start coding
     }
 }
