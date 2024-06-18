ce8ca42ec3a57c4a89d5e02390fe742b0b730003
nettahalamish <bygneta1611@gmail.com>
2024-06-18 12:08:43 +0300

    group 1

--- src/main/java/groups/Group1.java
+++ src/main/java/groups/Group1.java
@@ -18,7 +18,7 @@ public class Group1 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(1.7); //todo remove this line and start coding
+        moveForwardInSeconds(1.5); //todo remove this line and start coding
         turnRightInDegrees(90); //todo remove this line and start coding
         moveForwardInSeconds(0.5);
         turnLeftInDegrees(90);
