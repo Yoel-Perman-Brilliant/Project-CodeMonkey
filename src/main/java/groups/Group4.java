748596604a412e61579ca50cb5efbe98bc657fd9
Itay <itay2318@gmail.com>
2024-06-19 10:53:48 +0300

    group 2

--- src/main/java/groups/Group4.java
+++ src/main/java/groups/Group4.java
@@ -20,6 +20,5 @@ public class Group4 extends ProgramBase {
     public void writeProgram() {
         moveForwardInSeconds(2); //todo remove this line and start coding
         turnLeftInDegrees(90); //todo remove this line and start coding
-        turnLeftInDegrees(90); //todo remove this line and start coding
     }
 }
