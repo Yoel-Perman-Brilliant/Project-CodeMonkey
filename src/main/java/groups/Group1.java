def4b987fca3220a0f6b163d8c8574080d2ff100
spikes <spikes.no2212@gmail.com>
2024-12-12 16:06:59 +0200

    group 1

--- src/main/java/groups/Group1.java
+++ src/main/java/groups/Group1.java
@@ -18,7 +18,7 @@ public class Group1 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(3); //todo remove this line and start coding
+        moveForwardInSeconds(4); //todo remove this line and start coding
         turnRightInDegrees(90); //todo remove this line and start coding
         moveForwardInSeconds(3);
         turnLeftInDegrees(90);
