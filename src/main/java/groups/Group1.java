cc75a582287fa2b804fb92f37cc15597fd1ee795
turtleminecraft <schnitzelgirl69@gmail.com>
2024-12-12 16:47:13 +0200

    group 1

--- src/main/java/groups/Group1.java
+++ src/main/java/groups/Group1.java
@@ -20,7 +20,7 @@ public class Group1 extends ProgramBase {
     public void writeProgram() {
         moveForwardInSeconds(2); //todo remove this line and start coding
         turnRightInDegrees(90); //todo remove this line and start coding
-        moveForwardInSeconds(3);
+        moveForwardInSeconds(7);
         moveBackwardInSeconds(4);
         turnLeftInDegrees(20);
         turnRightInDegrees(50);
